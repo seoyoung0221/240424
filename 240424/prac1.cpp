675e70d631ed225fd8db63f320deff0b3e31771d
seoyoung0221 <jjan000221@gmail.com>
2024-04-24 15:42:26 +0900

    Add project files.

---
+++ 240424/prac1.cpp
@@ -0,0 +1,42 @@
+// 240424.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+int Add(int n1, int n2)
+{
+	return n1 + n2;
+}
+
+int Sub(int n1, int n2)
+{
+	return n1 - n2;
+}
+
+int Mul(int n1, int n2)
+{
+	return n1 * n2;
+}
+
+float Divide(int n1, int n2)
+{
+	float n_1 = n1;
+	float n_2 = n2;
+
+	return n_1 / n_2;
+}
+
+int main()
+{
+	std::cout << "2개의 숫자를 입력하세요.";
+	int n1, n2;
+	std::cin >> n1 >> n2;
+
+	std::cout << Add(n1, n2) << "\n";
+	std::cout << Sub(n1, n2) << "\n";
+	std::cout << Mul(n1, n2) << "\n";
+	std::cout << Divide(n1, n2) << "\n";
+
+	return 0;
+}
+
